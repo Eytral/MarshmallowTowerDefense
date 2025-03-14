b8276f71d18dda571816a9b5e447eb865bb18e0d
Eytral <today987789@gmail.com>
2025-03-15 00:17:43 +0800

    base enemy class fixed - pathfinding now works

--- Entities/Enemies/base_enemy.py
+++ Entities/Enemies/base_enemy.py
@@ -1,50 +1,64 @@
 from Constants import sprites, config
 
 class Enemy():
-    def __init__(self, reward, health, speed, position, path):
+    def __init__(self, reward, health, speed, start_position, path):
         self.reward = reward
         self.health = health
         self.speed = speed
-        self.position = position
+        self.position = start_position
         self.path = path
         self.is_dead = False
+        self.reached_end = False
         self.sprite = sprites.ENEMY_DEFAULT_SPRITE
         
     def move(self):
-        for target_position in self.path:
-            if self.position != target_position:
+        #print(self.position)
+        target_position = self.path[0]
+        print(f"targeting positiong: {target_position}")
 
-                x1, y1 = self.position
-                x2, y2 = target_position
-                
-                # Move horizontally
-                if x1 < x2:
-                    x1 += 1
-                elif x1 > x2:
-                    x1 -= 1
-                
-                # Move vertically
-                if y1 < y2:
-                    y1 += 1
-                elif y1 > y2:
-                    y1 -= 1
-                
-                # Update the enemy's position
-                self.position = (x1, y1)
+        x1, y1 = self.position
+        x2, y2 = target_position
+        
+        # Move horizontally
+        if x1 < x2:
+            x1 += self.speed
+        elif x1 > x2:
+            x1 -= self.speed
+        
+        # Move vertically
+        if y1 < y2:
+            y1 += self.speed
+        elif y1 > y2:
+            y1 -= self.speed
+        
+        # Update the enemy's position
+        self.position = (x1, y1)
+
+        if self.position == target_position:
+            print("found target")
+            del self.path[0]
 
 
     def draw(self, screen):
+        #print(f"enemy pos is:{self.position}")
         screen.blit(self.sprite, (self.position))
 
     def take_damage(self):
         self.health -= 1
 
     def check_is_dead(self):
-        if self.health >= 0:
+        if self.health <= 0:
             self.is_dead = True
+            print(f"Enemy has reached end (of its life)")
+
+    def check_has_reached_end(self):
+        if len(self.path) == 0:
+            self.reached_end = True
+            print(f"Enemy has reached end")
 
     def update(self):
-        self.check_is_dead()
-        self.take_damage()
         self.move()
+        self.check_is_dead()
+        self.check_has_reached_end()
+
         
