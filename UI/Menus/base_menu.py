d4a7534a74127407a7b6c119dad57d7f114f32d9
Eytral <today987789@gmail.com>
2025-03-14 09:52:47 +0800

    expanded base_menu parent class to reduce redundancy in subclasses - now shares the create_buttons function

--- UI/Menus/base_menu.py
+++ UI/Menus/base_menu.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
+from UI.button_class import Button
+from Constants import config
 
 class Menu(ABC):
-    def __init__(self, game):
+    def __init__(self, game, title, button_data):
         self.game = game
         self.buttons = []
+        self.title = title
+        self.create_buttons(button_data)
 
     def draw(self, screen):
         """Draw the menu and its buttons."""
@@ -13,3 +17,9 @@ class Menu(ABC):
     def add_button(self, button):
         """Add a button to the menu."""
         self.buttons.append(button)
+
+    def create_buttons(self, button_data):
+        x_pos = config.SCREEN_WIDTH//2 - config.BUTTON_WIDTH//2
+        for index, (text, action) in enumerate(button_data):
+            y_pos = config.BUTTON_DEFAULT_Y_POS + config.BUTTON_OFFSET*index
+            self.buttons.append(Button(text, (x_pos ,y_pos), action))
