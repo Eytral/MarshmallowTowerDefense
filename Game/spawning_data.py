ef75c80f005fc94f0ddfff61067953bcd5159dc9
Eytral <today987789@gmail.com>
2025-03-19 00:04:49 +0800

    basic spawning_data implemented - default enemy spawns, increments based on difficulties (3: Easy, Normal, Hard)

--- Game/spawning_data.py
+++ Game/spawning_data.py
@@ -5,19 +5,19 @@ SPAWNING_DATA = {
     "Easy": {
 
     "Default_Spawn": {
-        "Enemy 1": 3,
-        "Enemy 2": 2,
-        "Enemy 3": 0,
-        "Enemy 4": 0,
-        "Enemy 5": 0
+        "marshmallow enemy": 3,
+        "cracker enemy": 2,
+        "white_chocolate enemy": 0,
+        "dark_chocolate enemy": 0,
+        "smore enemy": 0
         },
 
     "Increment": {
-        "Enemy 1": 2,
-        "Enemy 2": 1,
-        "Enemy 3": 0.5,
-        "Enemy 4": 0.1,
-        "Enemy 5": 0.1
+        "marshmallow enemy": 2,
+        "cracker enemy": 1,
+        "white_chocolate enemy": 0.5,
+        "dark_chocolate enemy": 0.1,
+        "smore enemy": 0.1
     }
     },
 
@@ -26,19 +26,19 @@ SPAWNING_DATA = {
     "Normal": {
 
     "Default_Spawn": {
-        "Enemy 1": 4,
-        "Enemy 2": 3,
-        "Enemy 3": 1,
-        "Enemy 4": 0,
-        "Enemy 5": 0
+        "marshmallow enemy": 4,
+        "cracker enemy": 3,
+        "white_chocolate enemy": 1,
+        "dark_chocolate enemy": 0,
+        "smore enemy": 0
         },
 
     "Increment": {
-        "Enemy 1": 3,
-        "Enemy 2": 1.5,
-        "Enemy 3": 1,
-        "Enemy 4": 0.2,
-        "Enemy 5": 0.2
+        "marshmallow enemy": 3,
+        "cracker enemy": 1.5,
+        "white_chocolate enemy": 1,
+        "dark_chocolate enemy": 0.2,
+        "smore enemy": 0.2
     }
 
     },
@@ -48,20 +48,34 @@ SPAWNING_DATA = {
         "Hard": {
 
         "Default_Spawn": {
-        "Enemy 1"
-        "Enemy 2"
-        "Enemy 3"
-        "Enemy 4"
-        "Enemy 5"
+        "marshmallow enemy": 5,
+        "cracker enemy": 4,
+        "white_chocolate enemy": 1.5,
+        "dark_chocolate enemy": 0.5,
+        "smore enemy": 0.25
         },
 
         "Increment": {
 
-        "Enemy 1"
-        "Enemy 2"
-        "Enemy 3"
-        "Enemy 4"
-        "Enemy 5"
+        "marshmallow enemy": 3.5,
+        "cracker enemy": 2,
+        "white_chocolate enemy": 1.25,
+        "dark_chocolate enemy": 0.25,
+        "smore enemy": 0.25
     }
     },
+}
+
+from Entities.Enemies.marshmallow_enemy import Marshmallow
+from Entities.Enemies.cracker_enemy import Cracker
+from Entities.Enemies.white_chocolate_enemy import WhiteChocolate
+from Entities.Enemies.dark_chocolate_enemy import DarkChocolate
+from Entities.Enemies.smore_enemy import Smore
+
+ENEMY_CLASS_MAP = {
+    "marshmallow enemy": Marshmallow,
+    "cracker enemy": Cracker,
+    "white_chocolate enemy": WhiteChocolate,
+    "dark_chocolate enemy": DarkChocolate,
+    "smore enemy": Smore,
 }
