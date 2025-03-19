e09609d73695835b79a273f3b33b9561c9afd9e9
Eytral <today987789@gmail.com>
2025-03-19 22:42:45 +0800

    simplified enemy subclass default attributes

--- Entities/Enemies/white_chocolate_enemy.py
+++ Entities/Enemies/white_chocolate_enemy.py
@@ -2,6 +2,6 @@ from Entities.Enemies.base_enemy import Enemy
 from Constants import sprites, config
 
 class WhiteChocolate(Enemy):
-    def __init__(self, start_position, path, reward=5, health=10, speed=10):
-        super().__init__(start_position, path, reward, health, speed)
+    def __init__(self, start_position, path):
+        super().__init__(start_position, path)
         self.sprite = sprites.WHITE_CHOCOLATE_SPRITE
